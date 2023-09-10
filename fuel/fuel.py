fb794cfc4e2c437d89d85eac99919e3a309a2629
Michael Tague <michael@MichaelTague.com>
2023-09-10 17:41:09 +0000

    Sun, Sep 10, 2023, 1:41 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,11 +1,14 @@
 def main():
-    a = get_fraction("Fraction: ")
-    print(a)
+    x, y = get_fraction("Fraction: ")
+    print(x, y)
 
 def get_fraction(prompt):
     while True:
         try:
             x, y = input(prompt).split("/")
+            x = int(x)
+            y = int(y)
+            return x, y
         except ValueError:
             pass
 
