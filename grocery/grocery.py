6f2aa8ba232db17888f4986decb72f6b54d877b8
Michael Tague <michael@MichaelTague.com>
2023-09-10 22:28:02 +0000

    Sun, Sep 10, 2023, 6:28 PM -04:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,7 +6,12 @@ def main():
 
 def get_list():
     while True:
+        item = input()
+        list[item] = list.get(item, 0) + 1
         
+        if item in list:
+            value = list[item] + 1
+
 
 
 main()
