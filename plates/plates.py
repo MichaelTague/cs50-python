efa1dd5052a8e0d875c7db0bb2cd00f6c32c2a11
Michael Tague <michael@MichaelTague.com>
2023-09-10 06:12:28 +0000

    Sun, Sep 10, 2023, 2:12 AM -04:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,6 +9,15 @@ def main():
 def is_valid(s):
     if len(s) < 1 or len(s) > 6:
         return False
-
+    first_digit = True
+    for i in range(len(s)):
+        if i < 2 and s[i].isdigit():
+            return False
+        if s[i].isdigit():
+            if first_digit:
+                if s[i] == "0":
+                    return False
+                first_digit == false
+                
 
 main()
