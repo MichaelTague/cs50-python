a7763857f34953c5417f6ba419c6b53ff04b312d
Michael Tague <michael@MichaelTague.com>
2023-09-10 05:53:20 +0000

    Sun, Sep 10, 2023, 1:53 AM -04:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,8 @@
 amount = 50
-while amount != 0:
+while amount > 0:
     print("Amount Due:", amount)
     coin = input("Input Coin: ")
     match coin:
         case "25" | "10" | "5":
             amount = amount - int(coin)
+print("Change Owed:", - amount)
