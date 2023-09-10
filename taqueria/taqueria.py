d2376a8be78cd3bee54f19e3eec0a39954b4efa2
Michael Tague <michael@MichaelTague.com>
2023-09-10 21:33:23 +0000

    Sun, Sep 10, 2023, 5:33 PM -04:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,36 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+    x, y = get_fraction("Fraction: ")
+    percentage = x / y * 100
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{percentage:.0f}%")
+
+def get_item(prompt):
+    while True:
+        try:
+            item = input(prompt).title()
+
+
+            x = int(x)
+            y = int(y)
+            if y != 0 and x <= y:
+                return x, y
+        except ValueError:
+            pass
+
+main()
