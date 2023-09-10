25f7635ba29cefe56cae7273f528bc818aebc1f2
Michael Tague <michael@MichaelTague.com>
2023-09-10 17:43:59 +0000

    Sun, Sep 10, 2023, 1:43 PM -04:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -6,8 +6,14 @@ def get_fraction(prompt):
     while True:
         try:
             x, y = input(prompt).split("/")
+            if ! x.isnumeric() and y.isnumeric() and y != 0 and x
             x = int(x)
             y = int(y)
+            if y == 0:
+                continue
+            if x > y:
+                continue
+
             return x, y
         except ValueError:
             pass
