33ad9c85c591ddeab9a03cc3777348e2241d8941
Michael Tague <michael@MichaelTague.com>
2023-09-10 05:59:29 +0000

    Sun, Sep 10, 2023, 1:59 AM -04:00

---
+++ twttr/twttr.py
@@ -0,0 +1,9 @@
+string = input("Input: ")
+for c in string:
+    cl = c.lower()
+    match cl:
+        case "a" | "e" | "i" | "o" | "u":
+            continue
+        case _:
+            print(c, end="")
+print()
