bd32763085421e020b610f8abfa98c44e7f45166
Michael Tague <michael@MichaelTague.com>
2023-09-13 00:15:29 +0000

    Tue, Sep 12, 2023, 8:15 PM -04:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -4,8 +4,9 @@ def main():
     while True:
         try:
             sentence = input("Input: ")
-            print(emoji.emojize(sentence))
+            print(emoji.emojize(sentence, language='alias'))
         except EOFError:
+            print()
             return
 
 main()
