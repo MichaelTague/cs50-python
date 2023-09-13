cb55a14da2c3e936587077b0a7ba06aa0ba61fc9
Michael Tague <michael@MichaelTague.com>
2023-09-13 00:39:20 +0000

    Tue, Sep 12, 2023, 8:39 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,10 +3,18 @@ from pyfiglet import Figlet
 
 def main():
     if len(sys.argv) != 1 and len(sys.argv) != 3:
-        print("Error: Syntax: python pyfiglet.py ((-f | --font) <font-name>))")
+        print("Syntax: python pyfiglet.py ((-f | --font) <font-name>))")
         sys.exit()
 
+    figlet = Figlet()
+
     if len(sys.argv) == 1:
-        
+        font = random.choice(figlet.getFonts())
+    else:
+        if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+            print("Syntax: python pyfiglet.py ((-f | --font) <font-name>))")
+            sys.exit()
+
+
 
 print(len(sys.argv))
