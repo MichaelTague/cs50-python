665ea1793a1af7fa8b2dbdf414feb2b321ee6d0d
Michael Tague <michael@MichaelTague.com>
2023-09-13 00:35:43 +0000

    Tue, Sep 12, 2023, 8:35 PM -04:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,6 +2,7 @@ import sys
 from pyfiglet import Figlet
 
 if len(sys.argv) != 1 and len(sys.argv) != 3:
-    print("Error: Syntax: python pyfiglet.py ((-f | --font) ))
+    print("Error: Syntax: python pyfiglet.py ((-f | --font) <font-name>))")
+    sys.exit()
 
 print(len(sys.argv))
