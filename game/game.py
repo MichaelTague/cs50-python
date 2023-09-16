eaaeb14ffde21c9aa8f62f4dcbf6b0169cb7c2a4
Michael Tague <michael@MichaelTague.com>
2023-09-16 16:08:37 +0000

    Sat, Sep 16, 2023, 12:08 PM -04:00

--- game/game.py
+++ game/game.py
@@ -15,9 +15,13 @@ def main():
             print("Too large!")
         else:
             print("Just right!")
-            sys.exit()  # It is wild that the check routine for this
+           # sys.exit()  # It is wild that the check routine for this
                         # can tell the difference between a sys.exit() here
                         # versus a simple break which will allow a natural exit.
+                        # But it does!   The "check50 cs50/problems/2022/python/game"
+                        # kept saying I was never completing when clearly I was,
+                        # using sys.exit() fixed it.
+            break
 
 def get_natural_number(prompt):
     while True:
