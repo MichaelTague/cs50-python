a77d980de5a001b74db49d530dacd953a5fa6c84
Michael Tague <michael@MichaelTague.com>
2023-09-16 17:50:20 +0000

    Sat, Sep 16, 2023, 1:50 PM -04:00

--- lesson5/test_calculator.py
+++ lesson5/test_calculator.py
@@ -1,9 +1,11 @@
 from calculator import square
 
 
-def test_assert():
+def test_postive():
     assert square(2) == 4
     assert square(3) == 9
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+def test_zero():
     assert square(0) == 0
