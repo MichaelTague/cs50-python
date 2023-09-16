d052290861756ad9b785af0648730085d7dda8c8
Michael Tague <michael@MichaelTague.com>
2023-09-16 16:20:53 +0000

    Sat, Sep 16, 2023, 12:20 PM -04:00

---
+++ professor/professor.py
@@ -0,0 +1,19 @@
+import random
+
+
+def main():
+    ...
+
+
+def get_level():
+    while True:
+        level = input("Levels: ")
+        if level == "1" or level == "2" or level == "3":
+            return level
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
