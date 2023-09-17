798bf7fb7b48e5a818ef29f1f26dc02213c1ffb7
Michael Tague <michael@MichaelTague.com>
2023-09-17 17:35:45 +0000

    Sun, Sep 17, 2023, 1:35 PM -04:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,30 +3,30 @@ def main():
         try:
             fraction = input("Fraction: ")
             percentage = convert(fraction)
-            print("percentage", percentage)
             break
         except ValueError:
             pass
         except ZeroDivisionError:
             pass
-    if percentage <= 1:
-        print("E")
-    elif percentage >= 99:
-        print("F")
-    else:
-        print(f"{percentage:.0f}%")
-
+    print(gauge(percentage))
 
 def convert(fraction):
     x, y = fraction.split("/")
     x = int(x)
     y = int(y)
-    print(x, y)
     if x > y:
         raise ValueError
     if y == 0:
         raise ZeroDivisionError
     return int(x * 100.0 / y + .5)
+
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return str(percentage) + "%"
 
 if __name__ == "__main__":
     main()
