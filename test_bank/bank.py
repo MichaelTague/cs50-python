098bf8bf9242abcd5bb8e04f83b4dd28206738ea
Michael Tague <michael@MichaelTague.com>
2023-09-17 16:22:48 +0000

    Sun, Sep 17, 2023, 12:22 PM -04:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,14 +1,15 @@
 def main():
     response = input("Greeting: ").strip().lower()
-    print(value(response))
+    val = value(response)
+    print(f"${val}")
 
 def value(greeting):
     if greeting.startswith("hello"):
-        return "$0"
+        return 0
     elif greeting.startswith("h"):
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()
