d505d05420b3adbae996eea53bc8281dd5ca86d3
Michael Tague <michael@MichaelTague.com>
2023-09-17 22:24:48 +0000

    Sun, Sep 17, 2023, 6:24 PM -04:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,25 @@
+import sys
+import csv
+from tabulate import tabulate
+
+def main():
+    filename = get_csv_filename()
+    try:
+        with open(filename, "r") as file:
+            reader = csv.reader(file)
+            lines = list(reader)
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+    print(tabulate(lines, headers='firstrow', tablefmt="grid"))
+
+def get_csv_filename():
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    if not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
+    return sys.argv[1]
+
+if __name__ == "__main__":
+    main()
