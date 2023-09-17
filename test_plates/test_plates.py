7e8090e291d76445a210f29482c4963474d903ba
Michael Tague <michael@MichaelTague.com>
2023-09-17 17:15:24 +0000

    Sun, Sep 17, 2023, 1:15 PM -04:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -24,5 +24,9 @@ def test_too_long():
     assert is_valid("AAAAAA") == True
 
 def test_num_out_of_place():
-    assert is_valid("AA123BB") == False
-    assert is_valid("AABB123") == True
+    assert is_valid("AA12BB") == False
+    assert is_valid("AABB12") == True
+
+def test_bad_alpha():
+    assert is_valid("AB,12") == False
+    assert is_valid("AB12") == True
