1f39789405beffb42edb20e0fe249a54df6d84c5
Michael Tague <michael@MichaelTague.com>
2023-09-17 16:21:20 +0000

    Sun, Sep 17, 2023, 12:21 PM -04:00

---
+++ test_bank/test_bank.py
@@ -0,0 +1,10 @@
+from bank import value
+
+def test_hello():
+    assert value("hello there") == "$0"
+
+def test_h_word():
+    assert value("help me!") == "$20"
+
+def test_other():
+    assert value("76 trombones") == "$100"
