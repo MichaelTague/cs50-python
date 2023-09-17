a48b79ed91754b315b263ac5787df600f0cd2ebd
Michael Tague <michael@MichaelTague.com>
2023-09-17 17:16:54 +0000

    Sun, Sep 17, 2023, 1:16 PM -04:00

---
+++ test_fuel/fuel.py
@@ -0,0 +1,23 @@
+def main():
+    x, y = get_fraction("Fraction: ")
+    percentage = x / y * 100
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    else:
+        print(f"{percentage:.0f}%")
+
+def get_fraction(prompt):
+    while True:
+        try:
+            x, y = input(prompt).split("/")
+            x = int(x)
+            y = int(y)
+            if y != 0 and x <= y:
+                return x, y
+        except ValueError:
+            pass
+
+if __name_ == "__main__":
+    main()
