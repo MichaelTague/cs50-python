1ff93d7e4eaf46a9586ffab9d6052ce3008f4b0f
Michael Tague <michael@MichaelTague.com>
2023-09-17 16:07:35 +0000

    Sun, Sep 17, 2023, 12:07 PM -04:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,3 +5,9 @@ def test_lower():
 
 def test_upper():
     assert shorten("QUEUE") == "Q"
+
+def test_number():
+    assert shorten("10four") == "10fr"
+
+def test_punctuation():
+    assert shorten("10-four") == "10-fr"
