267a6162aff4e4d83459c276bdad232d275450b8
Michael Tague <michael@MichaelTague.com>
2023-09-17 18:36:06 +0000

    Sun, Sep 17, 2023, 2:36 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,6 +1,11 @@
 from fuel import convert, gauge
 import pytest
 
+def test_empty():
+    assert convert("1/200") == 1
+
 def test_value_error():
     with pytest.raises(ValueError):
         convert("cat/100")
+
+
