2a0b4d3b81cad514c83f10f73db99a2f32db2377
Michael Tague <michael@MichaelTague.com>
2023-09-17 22:31:43 +0000

    Sun, Sep 17, 2023, 6:31 PM -04:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,23 +3,26 @@ import csv
 from tabulate import tabulate
 
 def main():
-    filename = get_csv_filename()
+    file1, file2 = get_csv_filenames()
     try:
-        with open(filename, "r") as file:
+        with open(file1, "r") as file:
             reader = csv.reader(file)
-            lines = list(reader)
+            for something in reader:
+                print(something)
     except FileNotFoundError:
         sys.exit("Could not read " + filename)
-    print(tabulate(lines, headers='firstrow', tablefmt="grid"))
+    print(file1, file2)
 
-def get_csv_filename():
-    if len(sys.argv) == 1:
+def get_csv_filenames():
+    if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
     if not sys.argv[1].endswith(".csv"):
-        sys.exit("Not a CSV file")
-    return sys.argv[1]
+        sys.exit("Not a CSV file: " + sys.argv[1])
+    if not sys.argv[2].endswith(".csv"):
+        sys.exit("Not a CSV file: " + sys.argv[2])
+    return sys.argv[1], sys.argv[2]
 
 if __name__ == "__main__":
     main()
