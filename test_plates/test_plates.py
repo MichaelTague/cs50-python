860ecabbd5e651c5a909fcc55afbe3bc1ddbbc52
Michael Tague <michael@MichaelTague.com>
2023-09-17 16:49:54 +0000

    Sun, Sep 17, 2023, 12:49 PM -04:00

---
+++ test_plates/test_plates.py
@@ -0,0 +1,13 @@
+from bank import value
+
+def test_hello():
+    assert value("hello there") == 0
+
+def test_h_word():
+    assert value("help me!") == 20
+
+def test_other():
+    assert value("76 trombones") == 100
+
+def test_upper_case():
+    assert value("Hello") == 0
