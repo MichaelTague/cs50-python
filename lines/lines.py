a069a6ba361012bfa87831492896eb799aa93bb9
Michael Tague <michael@MichaelTague.com>
2023-09-17 20:09:12 +0000

    Sun, Sep 17, 2023, 4:09 PM -04:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,17 +1,19 @@
 import sys
 
 def main():
-    file = get_python_file()
+    filename = get_python_filename()
+    
 
 
-def get_python_file():
+
+def get_python_filename():
     if len(sys.argv) == 1:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
     if not sys.argv[1].endswith(".py"):
         sys.exit("Not a Python file")
-    with 
+    return sys.argv[1]
 
 if __name__ == "__main__":
     main()
