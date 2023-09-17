a29efcf04fc10508d570f5f27d692c43631e9361
Michael Tague <michael@MichaelTague.com>
2023-09-17 19:09:19 +0000

    Sun, Sep 17, 2023, 3:09 PM -04:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -18,7 +18,7 @@ def convert(fraction):
         raise ValueError
     if y == 0:
         raise ZeroDivisionError
-    return int(route(x * 100.0 / y))
+    return int(round(x * 100.0 / y))
 
 def gauge(percentage):
     if percentage <= 1:
