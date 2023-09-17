1c436488267f630d320bee82e1d975efc171d778
Michael Tague <michael@MichaelTague.com>
2023-09-17 17:41:23 +0000

    Sun, Sep 17, 2023, 1:41 PM -04:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,12 +2,12 @@ from fuel import convert, gauge
 
 def test_empty():
     assert convert("1/200") == 1
-    assert gauge("1") == "E"
+    assert gauge(1) == "E"
 
 def test_full():
     assert convert("199/200") == 100
-    assert gauge("100") == "F"
+    assert gauge(100) == "F"
 
 def test_middle():
     assert convert("50/100") == 50
-    assert gauge("50") == "50%"
+    assert gauge(50) == "50%"
