0e9a4377de618b735f07237d656cad661c3216bf
Michael Tague <michael@MichaelTague.com>
2023-09-17 22:06:19 +0000

    Sun, Sep 17, 2023, 6:06 PM -04:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -9,8 +9,6 @@ def main():
             lines = file.readlines()
     except FileNotFoundError:
         sys.exit("File does not exist")
-    count = get_count(lines)
-    print(count)
 
 def get_csv_filename():
     if len(sys.argv) == 1:
@@ -20,17 +18,6 @@ def get_csv_filename():
     if not sys.argv[1].endswith(".csv"):
         sys.exit("Not a CSV file")
     return sys.argv[1]
-
-def get_count(lines):
-    count = 0
-    for line in lines:
-        stripped_line = line.lstrip().rstrip()
-        if stripped_line.startswith("#"):
-            continue
-        if stripped_line == "":
-            continue
-        count += 1
-    return count
 
 if __name__ == "__main__":
     main()
