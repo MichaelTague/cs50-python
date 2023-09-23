081077aa6810cf51b36e5ad8a16ad673026d1633
Michael Tague <michael@MichaelTague.com>
2023-09-23 04:11:45 +0000

    Sat, Sep 23, 2023, 12:11 AM -04:00

--- working/working.py
+++ working/working.py
@@ -5,8 +5,8 @@ def main():
     print(convert(input("Hours: ")))
 
 def convert(s):
-    if match := re.search(r"^([0-9]{1,2})(?::([0-9]{2})) ([AP])M to ([0-9]{1,2})(?::([0-9]{2})) ([AP])M$"):
-        
+    if match := re.search(r"^([0-9]{1,2})(?::([0-9]{2}))? ([AP])M to ([0-9]{1,2})(?::([0-9]{2})) ([AP])M$"):
+
     raise ValueError
 
 if __name__ == "__main__":
