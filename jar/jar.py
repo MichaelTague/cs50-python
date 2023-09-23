699f60890fffdd0a204119b8dfa64ea17a6e74fe
Michael Tague <michael@MichaelTague.com>
2023-09-23 23:11:20 +0000

    Sat, Sep 23, 2023, 7:11 PM -04:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,7 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
-        self._capacity = capacity
-        self._size = 0
+        self.capacity = capacity
+        self.size = 0
 
     def __str__(self):
         return "🍪" * self.size
