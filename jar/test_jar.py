fbb3d67fd055015d5559c13f8de0f7cbd23b886b
Michael Tague <michael@MichaelTague.com>
2023-09-23 22:56:56 +0000

    Sat, Sep 23, 2023, 6:56 PM -04:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -3,7 +3,7 @@ import pytest
 from jar import Jar
 
 def test_init():
-    
+    pass
 
 def test_str():
     jar = Jar()
@@ -13,47 +13,8 @@ def test_str():
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
-
 def test_deposit():
-    ...
-
+    pass
 
 def test_withdraw():
-    ...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from jar import Jar
-
-def test_minutes_since():
-    assert minutes_since("2023-09-01", "2022-09-01") == 525600
-    assert minutes_since("2000-01-01", "1999-12-31") == 1440
-
-def test_int_to_minutes():
-    assert int_to_minutes_string(1440) == "One thousand, four hundred forty minutes"
-    assert int_to_minutes_string(525600) == "Five hundred twenty-five thousand, six hundred minutes"
-
-def test_invalid():
-    with pytest.raises(SystemExit) as exit_info:
-        minutes_since("2000-01-01", "Dec 31, 1999")
-    assert exit_info.value.args[0] == "Invalid date"
+    pass
