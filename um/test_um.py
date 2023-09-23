44ee78ed2347b1e1dd08dd9e1d29207b6f8a72f4
Michael Tague <michael@MichaelTague.com>
2023-09-23 16:48:06 +0000

    Sat, Sep 23, 2023, 12:48 PM -04:00

---
+++ um/test_um.py
@@ -0,0 +1,23 @@
+import pytest
+
+from um import count
+
+def test_no_um():
+    assert count("Hello, world!") == 0
+    assert count("The humnan race") == 0
+
+def test_leading_um():
+    assert count("Um, what's up?") == 1
+    assert count("Um, well, I'm just human.") == 1
+
+def test_embeded_um():
+    assert count("Well, um, I don't know.") == 1
+    assert count("I guess, um, I'm just human") == 1
+    assert count("But, I don't know um about you!") == 1
+
+def test_trailing_up():
+    assert count("Best two out of three?  Um?") == 1
+    assert count("No, you cheat too much.  um") == 1
+
+def test_multiple_ums():
+    assert count("Um, some people um just really can't talk um much without saying um.") == 4
