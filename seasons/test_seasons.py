b08e415ffa410f54363aff2fab92266382d48f4c
Michael Tague <michael@MichaelTague.com>
2023-09-23 22:26:59 +0000

    Sat, Sep 23, 2023, 6:26 PM -04:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -7,9 +7,9 @@ def test_minutes_since():
     assert minutes_since("2000-01-01", "1999-12-31") == 1440
 
 def test_int_to_minutes():
-    assert int_to_minutes_string(1440) = "One thousand, four hundred forty minutes"
-    assert int_to_minutes_string(525600) = "Five hundred twenty-five thousand, six hundred minutes"
+    assert int_to_minutes_string(1440) == "One thousand, four hundred forty minutes"
+    assert int_to_minutes_string(525600) == "Five hundred twenty-five thousand, six hundred minutes"
 
 def test_invalid():
     with pytest.raises(ValueError):
-        
+        minutes_since("2000-01-01", "Dec 31, 1999")
