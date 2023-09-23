59783dfd21957869c9049ec1e6360c2259715b7e
Michael Tague <michael@MichaelTague.com>
2023-09-23 05:57:20 +0000

    Sat, Sep 23, 2023, 1:57 AM -04:00

--- working/working.py
+++ working/working.py
@@ -5,7 +5,7 @@ def main():
     print(convert(input("Hours: ")))
 
 def convert(s):  # to ([0-9]{1,2})(?::([0-9]{2})) ([AP])M$
-    if match := re.search(r"^([0-9]{1,2})(?::([0-9]{2}))? ([AP])M", s):
+    if match := re.search(r"^([0-9]{1,2})(?::([0-9]{2}))? ([AP])M to ([0-9]{1,2})(?::([0-9]{2})) ([AP])M$", s):
         print(match.groups())
         # Validate
         if not (1 <= int(match[1]) <= 12):
