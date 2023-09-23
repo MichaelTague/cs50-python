8dc74d11c959f76b329147ac02196eec14c231a7
Michael Tague <michael@MichaelTague.com>
2023-09-23 23:59:25 +0000

    Sat, Sep 23, 2023, 7:59 PM -04:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,4 +1,15 @@
 from fpdf import FPDF
 
+def main():
+    pdf = pdf()
+    pdf.add_page()
+    pdf_set_font("Arial", size=20)
+    pdf.cell(100, 20, txt="Michael Tague", ln=True, align='C')
+    pdf.image("shirtificate.png", x=10, y=30, w=190)
+    pdf.output("shirtificate.pdf")
+
 name = input("Name: ")
 print(name)
+
+if __name__ == "__main__":
+    main()
