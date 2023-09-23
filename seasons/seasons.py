42eb1057fc3d26a3d928255539458577303d31f0
Michael Tague <michael@MichaelTague.com>
2023-09-23 21:33:47 +0000

    Sat, Sep 23, 2023, 5:33 PM -04:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,7 +4,8 @@ import inflect
 def main():
     dob = input("Dat of Birth: ")
     minutes = minutes_since(dob)
-    print(minutes)
+    p = inflect.engine()
+    print(p.number_to_words(minutes))
 
 def minutes_since(dob):
     return int((date.today() - date.fromisoformat(dob)).total_seconds() / 60)
