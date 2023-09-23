f91628caff810929448c9ba8f571594673821e75
Michael Tague <michael@MichaelTague.com>
2023-09-23 03:20:23 +0000

    Fri, Sep 22, 2023, 11:20 PM -04:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,21 +1,17 @@
 import re
 import sys
 
-
 def main():
     print(validate(input("IPv4 Address: ")))
-
 
 def validate(ip):
     if match := re.search(r"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$", ip):
         for i in range(1, 5):
-            if 0 <= match[i] <=
-        if 0 <= match1]
-
-
-
-...
-
+            print(match[i])
+            if not (0 <= int(match[i]) <= 255):
+                break
+        return True
+    return False
 
 if __name__ == "__main__":
     main()
