b5fa0774eb7b4a31c610751c235d58376bfdc8fd
Michael Tague <michael@MichaelTague.com>
2023-09-23 22:48:09 +0000

    Sat, Sep 23, 2023, 6:48 PM -04:00

--- jar/jar.py
+++ jar/jar.py
@@ -2,8 +2,8 @@ class Jar:
     def __init__(self, capacity=12):
         if not isinstance(capacity, int) or capacity < 0:
             raise ValueError
-        
-        ...
+        self.capacity = capacity
+        self.cookies = 0
 
     def __str__(self):
         ...
