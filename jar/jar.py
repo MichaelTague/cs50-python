211796c578082e80b581b00c60fa9b66a574f5c6
Michael Tague <michael@MichaelTague.com>
2023-09-23 23:31:04 +0000

    Sat, Sep 23, 2023, 7:31 PM -04:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,7 +1,7 @@
 class Jar:
     def __init__(self, capacity=12):
         self.capacity = capacity
-        self.size = -1
+        self.size = 0
 
     def __str__(self):
         return "🍪" * self._size
@@ -28,14 +28,14 @@ class Jar:
 
     @size.setter
     def size(self, cookies):
-        if cookies > self._capacity:
+        if cookies < 0 or cookies > self._capacity:
             raise ValueError
         self._size = cookies
 
 def main():
     jar = Jar()
     jar.deposit(3)
-    jar.withdraw(1)
+    jar.withdraw(3)
     print(jar)
 
 if __name__ == "__main__":
