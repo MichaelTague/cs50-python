dba12f06fd610aa59312a6c30e644a2e847d639d
Michael Tague <michael@MichaelTague.com>
2023-09-23 04:24:16 +0000

    Sat, Sep 23, 2023, 12:24 AM -04:00

--- working/working.py
+++ working/working.py
@@ -8,19 +8,27 @@ def convert(s):
     if match := re.search(r"^([0-9]{1,2})(?::([0-9]{2}))? ([AP])M to ([0-9]{1,2})(?::([0-9]{2})) ([AP])M$"):
         if not (1 <= int(match[1]) <= 12):
             raise ValueError
-        if match[2] != None:
-            if (1 <= int(match[2]) <= 12):
-                raise ValueError
+        if match[2] == None:
+            match[2] = "0"
+        elif (1 <= int(match[2]) <= 12):
+            raise ValueError
         if match[3] != "A" and match[3] != "P":
             raise ValueError
         if not (1 <= int(match[4]) <= 12):
             raise ValueError
-        if match[5] != None:
-            if (1 <= int(match[5]) <= 12):
-                raise ValueError
+        if match[5] == None:
+            match[5] = "0"
+        elif (1 <= int(match[5]) <= 12):
+            raise ValueError
         if match[6] != "A" and match[6] != "P":
             raise ValueError
-        time1 = f"{match[1]:02}"
+
+        if match[3] == "P":
+            match[1] = str(int(match[1]) + 12)
+        if match[6] == "P":
+            match[4] = str(int(match[4]) + 12)
+
+        time1 = f"{match[1]:02}:"
 
 
 
