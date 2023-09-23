fb043dd1e63f4002423b688766cdb1f85cb400a6
Michael Tague <michael@MichaelTague.com>
2023-09-23 03:39:31 +0000

    Fri, Sep 22, 2023, 11:39 PM -04:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,17 +1,11 @@
 import re
 import sys
 
-
 def main():
     print(parse(input("HTML: ")))
 
-
 def parse(s):
-    ...
-
-
-...
-
+    return re.search(r"https://www.youtube.com/embed/(.+)\"", s)
 
 if __name__ == "__main__":
     main()
