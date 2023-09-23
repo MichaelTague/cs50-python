c4e7f61241f3ed2b8e6d913f4dccd585ccf1f31f
Michael Tague <michael@MichaelTague.com>
2023-09-23 21:21:13 +0000

    Sat, Sep 23, 2023, 5:21 PM -04:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,7 +4,7 @@ def main():
     dob = input("Dat of Birth: ")
     today = date.today()
     birth = date.fromisoformat(dob)
-    print(today - birth)
+    print(type(today - birth))
 
 if __name__ == "__main__":
     main()
