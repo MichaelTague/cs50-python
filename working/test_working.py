6a2852f1a91f20d0122f8b736e6b545ac4fee905
Michael Tague <michael@MichaelTague.com>
2023-09-23 06:10:54 +0000

    Sat, Sep 23, 2023, 2:10 AM -04:00

--- working/test_working.py
+++ working/test_working.py
@@ -16,15 +16,5 @@ def test_bad_numbers():
     with pytest.raises(ValueError):
         convert("9 AM to 5:95 PM")
 
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        convert("4/0")
-
-
-    assert convert("009 AM to 5 PM") == "09:00 to 17:00"
-    assert convert("12 AM to 12 PM") == "00:00 to 12:00"
-
-def test_invalid_chars():
-    assert validate("a.2.3.4") == False
-    assert validate("1.2.c.4") == False
-    assert validate("1.2b.3.4") == False
+def test_hours():
+    assert conver("3 PM to 12 AM") == "15:00 to 00:00"
