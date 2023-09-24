6f40f606a6131424861be265c4a7e15bf329e918
Michael Tague <michael@MichaelTague.com>
2023-09-24 19:26:51 +0000

    Sun, Sep 24, 2023, 3:26 PM -04:00

--- project/project.py
+++ project/project.py
@@ -1,9 +1,10 @@
 def main():
-    pass
+    amount = 
 
 def payment(amount, interest, term):
     # P = a ÷ { [ (1 + r) n ] - 1 } ÷ [ r (1 + r) n]
-    
+    rate = interest/1200
+    payment = rate * amount / (1 - (1 + rate)** (-term))
 
 
 if __name__ == "__main__":
