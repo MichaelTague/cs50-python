e2612ce545c2d1e5e00dd7171eacf961d90bb5fe
Michael Tague <michael@MichaelTague.com>
2023-09-24 23:47:54 +0000

    Sun, Sep 24, 2023, 7:47 PM -04:00

--- project/project.py
+++ project/project.py
@@ -80,7 +80,7 @@ def calc_principal(interest: Decimal, term: int, payment: Decimal) -> Decimal:
     return rounding(principal, ROUND_UP)
 
 def calc_interest(principal: Decimal, term: int, payment: Decimal) -> Decimal:
-    interest = optimize.newton()
+#    interest = optimize.newton()
     interest = Decimal(0.01)
     return principal.quantize(Decimal('0.00'), rounding=ROUND_UP)
 
