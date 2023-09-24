5fbae150419e948b0c70414dc49ac3807a1861c2
Michael Tague <michael@MichaelTague.com>
2023-09-24 20:14:11 +0000

    Sun, Sep 24, 2023, 4:14 PM -04:00

--- project/project.py
+++ project/project.py
@@ -16,6 +16,8 @@ def calc_payment(amount: float, rate: float, term: int) -> float:
     return math.ceil(payment * 100) / 100
 
 def final_payment(amount, rate, term, payment):
+    total_interest = 0
+    
     for i in range(1, term + 1):
         interest_portion = round(amount * rate, 2)
         reduction_portion = payment - interest_portion
@@ -23,7 +25,8 @@ def final_payment(amount, rate, term, payment):
             reduction_portion = amount
             payment = amount + interest_portion
         amount -= reduction_portion
-        print(f"{i:3}  {payment:9.2f}   {interest_portion:9.2f}   {reduction_portion:9.2f}   {amount:9.2f}")
+#        print(f"{i:3}  {payment:9.2f}   {interest_portion:9.2f}   {reduction_portion:9.2f}   {amount:9.2f}")
+    return i, payment,
 
 if __name__ == "__main__":
     main()
