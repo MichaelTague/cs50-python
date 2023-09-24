34346e07f8239c0edef3b916d55e750d70cb0073
Michael Tague <michael@MichaelTague.com>
2023-09-24 23:04:58 +0000

    Sun, Sep 24, 2023, 7:04 PM -04:00

--- project/project.py
+++ project/project.py
@@ -25,6 +25,7 @@ def main():
     elif payment == None:
         payment = calc_payment(principal, interest, term)
         print(f"Monthly payment: ${payment:,.2f}")
+        print(final_payment(principal, interest, term, payment))
 
 #    print(final_payment(principal, interest, term, payment))
 #    print(final_payment(principal, interest, term, rounding(payment + Decimal(.01))))
@@ -73,26 +74,26 @@ def calc_payment(principal: Decimal, interest: Decimal, term: int) -> Decimal:
     payment: Decimal = interest * principal / (1 - (1 + interest)**(-term))
     return rounding(payment, ROUND_UP)
 
-def final_payment(amount: Decimal, rate: Decimal, term: int, payment: Decimal, table=False):
+def final_payment(principal: Decimal, interest: Decimal, term: int, payment: Decimal, table=False):
     first_payment = payment
     total_interest = Decimal(0)
     total_principal = Decimal(0)
     if table:
         print(f"{0:5,}                                                     {amount:14,.2f}")
     for i in range(1, term + 1):
-        if amount == Decimal(0):
+        if principal == Decimal(0):
             break
-        interest_portion = rounding(amount * rate, ROUND_HALF_UP)
+        interest_portion = rounding(principal * interest, ROUND_HALF_UP)
         reduction_portion = payment - interest_portion
-        if reduction_portion > amount:
-            reduction_portion = amount
-            payment = amount + interest_portion
-        amount -= reduction_portion
+        if reduction_portion > principal:
+            reduction_portion = principal
+            payment = principal + interest_portion
+        principal -= reduction_portion
         total_interest += interest_portion
         total_principal += reduction_portion
         if table:
-            print(f"{i:5,}  {payment:14,.2f}   {interest_portion:14,.2f}   {reduction_portion:14,.2f}   {amount:14,.2f}")
-    return {"#": i, "payment": first_payment, "last payment": payment, "remaining": amount, "total interest": total_interest, "total principal": total_principal}
+            print(f"{i:5,}  {payment:14,.2f}   {interest_portion:14,.2f}   {reduction_portion:14,.2f}   {principal:14,.2f}")
+    return {"#": i, "payment": first_payment, "last payment": payment, "remaining": principal, "total interest": total_interest, "total principal": total_principal}
 
 if __name__ == "__main__":
     main()
