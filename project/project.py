6b647dd5496397a0fc7cae54f184dff4e77805b0
Michael Tague <michael@MichaelTague.com>
2023-09-24 23:20:04 +0000

    Sun, Sep 24, 2023, 7:20 PM -04:00

--- project/project.py
+++ project/project.py
@@ -65,7 +65,8 @@ def print_final_payment(d: dict):
     total_principal = d["total principal"]
     total_payments = total_interest + total_principal
     remaining = d["remaining"]
-    print(type(last))
+    print(type(term))
+    print()
     print(f"Last payment: {last:14,.2} at payment {term:1,}, total interest: {total_interest:1,.2}, total payments: {total_payments:1,.2}, balloon (if any): {remaining:1,.2}")
 
 def calc_principal(interest: Decimal, term: int, payment: Decimal) -> Decimal:
