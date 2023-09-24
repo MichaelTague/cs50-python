f0022397b026bf93587d00ed379a08f40697e3a6
Michael Tague <michael@MichaelTague.com>
2023-09-24 19:44:04 +0000

    Sun, Sep 24, 2023, 3:44 PM -04:00

--- project/project.py
+++ project/project.py
@@ -4,11 +4,11 @@ def main():
     amount = float(input("Amount: "))
     interest = float(input("Interest per annum: "))
     term = int(input("Term (in months): "))
-    monthly = math.floor(payment(amount, interest, term) * 100) / 100
-    print(payment(amount, interest, term))
+    payment = math.floor(calc_payment(amount, interest, term) * 100) / 100
+    print(calc_payment(amount, interest, term))
     print(f"Monthly payment: {monthly:.2f}", math.floor(amount))
 
-def payment(amount: float, interest: float, term: int) -> float:
+def calc_payment(amount: float, interest: float, term: int) -> float:
     # P = a ÷ { [ (1 + r) n ] - 1 } ÷ [ r (1 + r) n]
     rate = interest/1200
     payment = rate * amount / (1 - (1 + rate)**(-term))
