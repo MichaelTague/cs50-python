2197bff7f8d0fe7e54ba516ce690daeed35b5b8b
Michael Tague <michael@MichaelTague.com>
2023-09-24 20:56:14 +0000

    Sun, Sep 24, 2023, 4:56 PM -04:00

--- project/project.py
+++ project/project.py
@@ -22,6 +22,7 @@ def calc_payment(amount: Decimal, rate: Decimal, term: int) -> Decimal:
 def final_payment(amount, rate, term, payment):
     total_interest = Decimal(0)
     total_principal = Decimal(0)
+    print(f"{0:5,}                                                     {amount:14,.2f}")
     for i in range(1, term + 1):
         interest_portion = rounding(amount * rate, ROUND_HALF_UP)
         reduction_portion = payment - interest_portion
@@ -31,7 +32,7 @@ def final_payment(amount, rate, term, payment):
         amount -= reduction_portion
         total_interest += interest_portion
         total_principal += reduction_portion
-        print(f"{i:5,}  {payment:9,.2f}   {interest_portion:9,.2f}   {reduction_portion:9,.2f}   {amount:9,.2f}")
+        print(f"{i:5,}  {payment:14,.2f}   {interest_portion:14,.2f}   {reduction_portion:14,.2f}   {amount:14,.2f}")
     return i, payment, amount, total_interest, total_principal
 
 if __name__ == "__main__":
