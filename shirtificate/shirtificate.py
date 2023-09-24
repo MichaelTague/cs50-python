5e48c697bf66e7d2047e8cae7384d6128c20d2fe
Michael Tague <michael@MichaelTague.com>
2023-09-24 15:50:35 +0000

    Sun, Sep 24, 2023, 11:50 AM -04:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,6 +8,7 @@ def main():
     pdf.set_font("Arial", size=20)
     pdf.cell(txt="CS50 Shirtificate", center=True)
     pdf.set_text_color(255, 255, 255)
+#    pdf.set_y(297/2)
     pdf.cell(txt=name + "took CS50", center=True)
     pdf.image("shirtificate.png", x=0.5, y=60)
     pdf.output("shirtificate.pdf")
