a37953f758ead04947fde835df3e75635b760ec3
Michael Tague <michael@MichaelTague.com>
2023-09-24 15:31:21 +0000

    Sun, Sep 24, 2023, 11:31 AM -04:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,6 +3,7 @@ from fpdf import FPDF
 def main():
     name = input("Name: ")
     pdf = FPDF(orientation="portrait", format="A4")
+    pdf.set_auto_page_break(auto=False)
     pdf.add_page()
     pdf.set_font("Arial", size=20)
     pdf.cell(txt="CS50 Shirtificate", center=True)
