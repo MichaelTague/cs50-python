ceb213f6822a8efe7cc51d888d75bdd9099de2be
Michael Tague <michael@MichaelTague.com>
2023-09-30 19:10:47 +0000

    Sat, Sep 30, 2023, 3:10 PM -04:00

--- project/project.py
+++ project/project.py
@@ -1,70 +1,96 @@
+import re
 import sys
 import math
 import decimal
 from decimal import Decimal, ROUND_UP, ROUND_HALF_UP, ROUND_DOWN
-import numpy as np
 from scipy import optimize
 
 ctx = decimal.getcontext()
 ctx.rounding = ROUND_HALF_UP
-print(decimal.getcontext())
+#print(decimal.getcontext())
+
 
 def rounding(value: Decimal, round_type=ROUND_DOWN) -> Decimal:
     return value.quantize(Decimal('0.00'), rounding=round_type)
 
 def main():
-    print("Input any 3 out of 4")
-    principal_str = input("Principal: ")
-    interest_str = input("Interest per annum: ")
-    term_str = input("Term (in months): ")
+    print("Input three answers for the following four questions, the empty answer will be calaulated.")
+    principal_str = input("Principal Amount: ")
+    interest_str = input("Interest per Annum: ")
+    term_str = input("Term in Years (or write \"months\"): ")
     payment_str = input("Monthly Payment: ")
 
-    principal, interest, term, payment = validate_input(principal_str, interest_str, term_str, payment_str)
+    principal, interest, term, payment = convert_input(principal_str, interest_str, term_str, payment_str)
 
     if principal == None:
         principal = calc_principal(interest, term, payment)
-        print(f"Principal: ${principal:,.2f}")
     elif interest == None:
-        interest = 0
+        interest: float = calc_interest(principal, term, payment)
     elif term == None:
         term = calc_term(principal, interest, payment)
-        print(f"Term: {term:,.2f}")
     elif payment == None:
         payment = calc_payment(principal, interest, term)
-        print(f"Monthly payment: ${payment:,.2f}")
-        print_final_payment(final_payment(principal, interest, term, payment, table=False))
-        print(decimal.getcontext())
+    print_loan(principal, interest, term, payment)
 
-#    print(final_payment(principal, interest, term, payment))
-#    print(final_payment(principal, interest, term, rounding(payment + Decimal(.01))))
+def print_loan(principal: Decimal, interest: Decimal, term: int, payment: Decimal):
+    interest *= 1200
+    term = pretty_term(term)
+    print(f"Loan Principal: ${principal:,.2f}")
+    print(f"Loan Interest:  {interest}%")
+    print(f"Loan Term:      {term}")
+    print(f"Loan Payment:   ${payment:,.2f}")
 
-def validate_input(principal_str, interest_str, term_str, payment_str):
-    empties = 0
+def pretty_term(term: int):
+    years = term // 12
+    months = term % 12
+    if years == 0:
+        return str(term) + " Months"
+    if months == 0:
+        return str(years) + " Years"
+    return str(years) + " Years, " + str(months) + " Months"
+
+def convert_input(principal_str, interest_str, term_str, payment_str):
+    provided = 0
     principal = interest = term = payment = None
     try:
-        if principal_str == "":
-            empties += 1
-        else:
+        if principal_str != "":
             principal = Decimal(principal_str)
-        if interest_str == "":
-            empties += 1
-        else:
+            provided += 1
+        if interest_str != "":
             interest = Decimal(interest_str) / Decimal(1200)
-        if term_str == "":
-            empties += 1
-        else:
-            term = int(term_str)
-        if payment_str == "":
-            empties += 1
-        else:
+            provided += 1
+        if term_str != "":
+            term = parse_term_str(term_str)
+            provided += 1
+        if payment_str != "":
             payment = Decimal(payment_str)
+            provided += 1
     except decimal.InvalidOperation:
         sys.exit("Principal, Interest, and Payment must be Integer or Decimal Number")
     except ValueError:
-        sys.exit("Term can only be an Integer")
-    if empties != 1:
+        sys.exit("Term must be greater than zero and similar to these:  30 yrs; 60 months; 3 years, 6 months")
+    if provided != 3:
         sys.exit("One, and only one, of Principal, Interest, Term, and Payment should be left empty")
     return principal, interest, term, payment
+
+def parse_term_str(term_str: str):
+    years: int = 0
+    months: int = 0
+    matches = re.findall(r'(\d+)\s*(\w+)?', term_str)
+    for match in matches:
+        number, unit = match
+        if unit == "":
+            years = int(number)
+        elif re.search(r'(yr|year)s?', unit, re.IGNORECASE):
+            years = int(number)
+        elif re.search(r'(mo|month)s?', unit, re.IGNORECASE):
+            months = int(number)
+        else:
+            raise ValueError("Invalid Term format")
+    term: int = years * 12 + months
+    if term <= 0:
+        raise ValueError("Zero term")
+    return term
 
 def print_final_payment(d: dict):
     term = d["#"]
@@ -80,18 +106,28 @@ def calc_principal(interest: Decimal, term: int, payment: Decimal) -> Decimal:
     return rounding(principal, ROUND_UP)
 
 def calc_interest(principal: Decimal, term: int, payment: Decimal) -> Decimal:
-#    interest = optimize.newton()
-    interest = Decimal(0.01)
-    return principal.quantize(Decimal('0.00'), rounding=ROUND_UP)
+    interest = float(0.006) # 6% guess, then use Newton's method to find the a better guess
+    interest = optimize.newton(lambda x: float(calc_unrounded_payment(principal, Decimal(x), term) - payment), interest)
+    interest = Decimal(interest)
+    return rounding(interest, ROUND_HALF_UP)
 
 def calc_term(principal: Decimal, interest: Decimal, payment: Decimal) -> Decimal:
     term = - math.log(1 - (principal * interest / payment)) / math.log(1 + interest)
     return rounding(term, ROUND_DOWN)
 
 def calc_payment(principal: Decimal, interest: Decimal, term: int) -> Decimal:
+    payment = calc_unrounded_payment(principal, interest, term)
+    payment = rounding(payment, ROUND_UP)
+    payment = adjust_payment_for_final(principal, interest, payment, term)
+    return payment
+
+def adjust_payment_for_final(principal: Decimal, interest: Decimal, payment: Decimal, term: int):
+    return payment
+
+def calc_unrounded_payment(principal: Decimal, interest: Decimal, term: int) -> Decimal:
     payment: Decimal = interest * principal / (1 - (1 + interest)**(-term))
-    print(payment)
-    return rounding(payment, ROUND_UP)
+    print(interest, payment)
+    return Decimal(payment)
 
 def final_payment(principal: Decimal, interest: Decimal, term: int, payment: Decimal, table=False):
     first_payment = payment
