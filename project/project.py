c339240b2ee5bb958281566da4d9b5bdeffba5e1
Michael Tague <michael@MichaelTague.com>
2023-10-01 03:15:21 +0000

    Sat, Sep 30, 2023, 11:15 PM -04:00

--- project/project.py
+++ project/project.py
@@ -197,6 +197,8 @@ def adjust_interest(principal: Decimal, interest: Decimal, term: int, payment: D
             interest_add -= ONE_CENT
             new_interest = (annual_interest + interest_add) / Decimal(1200)
             new_final = final_payment(principal, new_interest, term, payment)
+            print("interest_add", interest_add)
+            print("new_interest", new_interest)
             print(new_final)
             if new_final['remaining'] != ZERO_CENTS:
                 return old_interest
