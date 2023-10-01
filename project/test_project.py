8d9175f42657b9a80f65034cc6e4039b22285ee8
Michael Tague <michael@MichaelTague.com>
2023-10-01 20:02:51 +0000

    Sun, Oct 1, 2023, 4:02 PM -04:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,11 +1,13 @@
 import pytest
 
-import project
+from project import pretty_term
 
 def test_pretty_term():
     assert pretty_term(360) == "30 Years"
     assert pretty_term(363) == "30 Years, 3 Months"
-    assert pretty_term(1) == "1 Month"
-    assert pretty_term(2) == "2 Months"
-    assert pretty_term(12) == "1 Year"
-    assert pretty_term(13) == "1 Year, 1 Month"
+    assert pretty_term(1)   == "1 Month"
+    assert pretty_term(2)   == "2 Months"
+    assert pretty_term(12)  == "1 Year"
+    assert pretty_term(13)  == "1 Year, 1 Month"
+
+
