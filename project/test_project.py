26b229702072209f209f5881b39e7b8d9c9f32e1
Michael Tague <michael@MichaelTague.com>
2023-10-01 20:08:07 +0000

    Sun, Oct 1, 2023, 4:08 PM -04:00

--- project/test_project.py
+++ project/test_project.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project import pretty_term
+from project import pretty_term, parse_term_str
 
 def test_pretty_term():
     assert pretty_term(360) == "30 Years"
@@ -10,4 +10,17 @@ def test_pretty_term():
     assert pretty_term(12)  == "1 Year"
     assert pretty_term(13)  == "1 Year, 1 Month"
 
+def test_parse_term_string():
+    assert parse_term_str("30") == 360
+    assert parse_term_str("30 yr") == 360
+    assert parse_term_str("30 yrs") == 360
+    assert parse_term_str("30 Year") == 360
+    assert parse_term_str("30 years") == 360
+    assert parse_term_str("30 mo") == 30
+    assert parse_term_str("30 mos") == 30
+    assert parse_term_str("30 Month") == 30
+    assert parse_term_str("30 months") == 30
+    assert parse_term_str("5 yr 6 mo") == 66
+    assert parse_term_str("16 Years, 8 Months") == 200
+    assert parse_term_str("100 yr") == 1200
 
